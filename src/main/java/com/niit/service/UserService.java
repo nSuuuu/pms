77e4cece9363ffadd9f89c1a5ce2f979316f3251
Coco2551 <2639726519@qq.com>
2025-06-24 16:11:50 +0800

    Create UserService.java

---
+++ src/main/java/com/niit/service/UserService.java
@@ -0,0 +1,45 @@
+package com.niit.service;
+
+import com.niit.entity.User;
+import com.niit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        if (user.getId() == null) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    public boolean isUsernameExists(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public boolean isPhoneExists(String phone) {
+        return userRepository.existsByPhone(phone);
+    }
+}
