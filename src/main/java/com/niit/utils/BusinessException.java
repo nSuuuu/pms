3baee0f954fb711a978946cb19bb8fbf5cdb27d5
j1rug <1120361164@qq.com>
2025-06-24 11:23:38 +0800

    Update BusinessException.java

--- src/main/java/com/niit/utils/BusinessException.java
+++ src/main/java/com/niit/utils/BusinessException.java
@@ -1,6 +1,6 @@
 package com.niit.utils;
 
-public class BusinessException extends Exception {
+public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
     }
