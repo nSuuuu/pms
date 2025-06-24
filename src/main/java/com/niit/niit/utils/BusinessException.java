6f22ec64e409ac2933ebaf2ca6116e254cf2c9dc
nSuuuu <164289995+nSuuuu@users.noreply.github.com>
2025-06-24 15:26:39 +0800

    Delete src directory

--- src/main/java/com/niit/niit/utils/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.niit.niit.utils;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/niit/service/ChatService.java
+++
@@ -1,9 +0,0 @@
-package com.niit.service;
-
-import com.niit.entity.ChatMessage;
-import java.util.List;
-
-public interface ChatService {
-    ChatMessage sendMessage(Integer fromUserId, Integer toUserId, String content);
-    List<ChatMessage> getChatHistory(Integer userId1, Integer userId2);
-} 
